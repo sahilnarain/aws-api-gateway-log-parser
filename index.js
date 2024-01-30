baec7de53b34bb8a325b9fe4f90770e76881a2b6
Sahil Narain <sahil.narain@gmail.com>
2024-01-30 21:40:38 +0000

    Handle non-json types for Type3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
-const groupBy = require("lodash.groupby");
+const groupBy = require('lodash.groupby');
 
-const _parseType1 = data => {
+const _parseType1 = (data) => {
   let result = {};
 
   if (!data) {
@@ -11,12 +11,12 @@ const _parseType1 = data => {
 
   let _objects = data.split(/\,\ /);
 
-  _objects.forEach(object => {
-    let _result = object.trim().split("=");
-    if (_result[0] && _result[1] && _result[1].split(",").length === 1) {
+  _objects.forEach((object) => {
+    let _result = object.trim().split('=');
+    if (_result[0] && _result[1] && _result[1].split(',').length === 1) {
       result[_result[0]] = _result[1];
-    } else if (_result[0] && _result[1] && _result[1].split(",").length > 1) {
-      result[_result[0]] = _result[1].split(",");
+    } else if (_result[0] && _result[1] && _result[1].split(',').length > 1) {
+      result[_result[0]] = _result[1].split(',');
     }
   });
 
@@ -27,39 +27,25 @@ const _parseType2 = (config, data) => {
   let result = {};
   let _truncatedRegEx = /\[TRUNCATED\]/;
 
-  if (
-    _truncatedRegEx.test(data) &&
-    config.TRUNCATED_RESPONSE_KEYS &&
-    config.TRUNCATED_RESPONSE_KEYS.length
-  ) {
-    config.TRUNCATED_RESPONSE_KEYS.forEach(obj => {
+  if (_truncatedRegEx.test(data) && config.TRUNCATED_RESPONSE_KEYS && config.TRUNCATED_RESPONSE_KEYS.length) {
+    config.TRUNCATED_RESPONSE_KEYS.forEach((obj) => {
       let _regEx = new RegExp(`"${obj.key}"(:"?)(.*?)("?)(\,|})`);
-      let _conversion = "String";
-      if (obj.type === "Integer" || obj.type === "Float") {
-        _conversion = "Number";
-      } else if (obj.type === "Boolean") {
-        _conversion = "Boolean";
+      let _conversion = 'String';
+      if (obj.type === 'Integer' || obj.type === 'Float') {
+        _conversion = 'Number';
+      } else if (obj.type === 'Boolean') {
+        _conversion = 'Boolean';
       }
 
       result[obj.key] =
-        data.match(_regEx) && data.match(_regEx).length ?
-        _conversion === "Boolean" ?
-        eval(
-          _conversion +
-          "(" +
-          eval(JSON.stringify(data.match(_regEx)[2])) +
-          ")"
-        ) :
-        eval(
-          _conversion + "(" + JSON.stringify(data.match(_regEx)[2]) + ")"
-        ) :
-        null;
+        data.match(_regEx) && data.match(_regEx).length
+          ? _conversion === 'Boolean'
+            ? eval(_conversion + '(' + eval(JSON.stringify(data.match(_regEx)[2])) + ')')
+            : eval(_conversion + '(' + JSON.stringify(data.match(_regEx)[2]) + ')')
+          : null;
       result.truncated = true;
     });
-  } else if (
-    _truncatedRegEx.test(data) &&
-    (!config.TRUNCATED_RESPONSE_KEYS || !config.TRUNCATED_RESPONSE_KEYS)
-  ) {
+  } else if (_truncatedRegEx.test(data) && (!config.TRUNCATED_RESPONSE_KEYS || !config.TRUNCATED_RESPONSE_KEYS)) {
     result = {
       truncated: true
     };
@@ -74,28 +60,42 @@ const _parseType2 = (config, data) => {
   return result;
 };
 
-const _parseType3 = data => {
+const _parseType3 = (data) => {
   let result = {};
 
   if (!data) {
     return result;
   }
 
-  let _objects = data.split(/\,/);
+  if (data.includes('{')) {
+    let _objects = data.split(/\,/);
 
-  _objects.forEach(object => {
-    let _result = object.trim().replace(/\{|\}/, '').split(":");
-    if (_result[0] && _result[1] && _result[1].split(",").length === 1) {
-      result[_result[0]] = _result[1];
-    } else if (_result[0] && _result[1] && _result[1].split(",").length > 1) {
-      result[_result[0]] = _result[1].split(",");
+    _objects.forEach((object) => {
+      let _result = object.trim().replace(/\{|\}/, '').split(':');
+      if (_result[0] && _result[1] && _result[1].split(',').length === 1) {
+        result[_result[0]] = _result[1];
+      } else if (_result[0] && _result[1] && _result[1].split(',').length > 1) {
+        result[_result[0]] = _result[1].split(',');
+      }
+    });
+  } else if (data.includes('=')) {
+    let objects = data.split('&');
+
+    let _result = [];
+
+    for (let object of objects) {
+      _result.push(...object.split('='));
     }
-  });
+
+    for (let i = 0; i < _result.length; i += 2) {
+      result[_result[i]] = _result[i + 1];
+    }
+  }
 
   return result;
 };
 
-const _parseKeyThrottle = data => {
+const _parseKeyThrottle = (data) => {
   const result = {};
 
   const limitMatches = data.match(/Limit:\s([0-9.]+)/);
@@ -158,48 +158,44 @@ const parseSingleRequest = (config, logEvents) => {
         _captureGroup = l.message.match(parseExps[exp])[1];
 
         switch (exp) {
-        case "request_query_string":
-        case "endpoint_request_headers":
-        case "endpoint_response_headers":
-        case "method_response_headers":
-        case "method_request_headers":
-          result[exp] = _parseType1(_captureGroup);
-          break;
+          case 'request_query_string':
+          case 'endpoint_request_headers':
+          case 'endpoint_response_headers':
+          case 'method_response_headers':
+          case 'method_request_headers':
+            result[exp] = _parseType1(_captureGroup);
+            break;
 
-        case "endpoint_response_body":
-        case "method_response_body":
-          result[exp] = _parseType2(config, _captureGroup);
-          break;
+          case 'endpoint_response_body':
+          case 'method_response_body':
+            result[exp] = _parseType2(config, _captureGroup);
+            break;
 
-        case "method_request_body":
-        case "endpoint_request_body":
-          result[exp] = _parseType3(_captureGroup);
-          break;
+          case 'method_request_body':
+          case 'endpoint_request_body':
+            result[exp] = _parseType3(_captureGroup);
+            break;
 
-        case "integration_latency":
-        case "method_status":
-          result[exp] = Number(_captureGroup);
-          break;
-        case "key_throttle":
-          result[exp] = _parseKeyThrottle(_captureGroup);
-          break;
+          case 'integration_latency':
+          case 'method_status':
+            result[exp] = Number(_captureGroup);
+            break;
+          case 'key_throttle':
+            result[exp] = _parseKeyThrottle(_captureGroup);
+            break;
 
-        default:
-          result[exp] = _captureGroup;
-          break;
+          default:
+            result[exp] = _captureGroup;
+            break;
         }
       }
     }
   }
 
-  result["request-start-time"] = new Date(logEvents[0].timestamp);
-  result["request-end-time"] = new Date(
-    logEvents[logEvents.length - 1].timestamp
-  );
-  result["request-execution-duration"] =
-    result["request-end-time"].getTime() -
-    result["request-start-time"].getTime();
-  result["@timestamp"] = result["request-start-time"].toISOString();
+  result['request-start-time'] = new Date(logEvents[0].timestamp);
+  result['request-end-time'] = new Date(logEvents[logEvents.length - 1].timestamp);
+  result['request-execution-duration'] = result['request-end-time'].getTime() - result['request-start-time'].getTime();
+  result['@timestamp'] = result['request-start-time'].toISOString();
 
   return result;
 };
@@ -213,7 +209,7 @@ const parseLogs = (config, logs) => {
     return null;
   }
 
-  if (logs.messageType !== "DATA_MESSAGE") {
+  if (logs.messageType !== 'DATA_MESSAGE') {
     return null;
   }
 
@@ -221,12 +217,9 @@ const parseLogs = (config, logs) => {
     return null;
   }
 
-  const logEventsByRequestId = groupBy(
-    logs.logEvents,
-    logEvent => logEvent.message.match(/\(([0-9a-zA-Z-]+)\)/)[1]
-  );
+  const logEventsByRequestId = groupBy(logs.logEvents, (logEvent) => logEvent.message.match(/\(([0-9a-zA-Z-]+)\)/)[1]);
 
-  return Object.keys(logEventsByRequestId).map(requestId => {
+  return Object.keys(logEventsByRequestId).map((requestId) => {
     const logEvents = logEventsByRequestId[requestId];
 
     return parseSingleRequest(config, logEvents);
